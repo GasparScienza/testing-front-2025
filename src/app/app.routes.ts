ed110b2f421d18590f8aee1ea618e0ae0e7fb877
manuNamuncura <manu.namun94@gmail.com>
2025-09-05 15:42:53 -0300

    feat: se agrego nueva ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {LandingPage} from './landing-page/landing-page';
 import {Register} from './register/register';
 import {Login} from './login/login';
+import { CalendarioTurnosComponent } from './calendario-turnos/calendario-turnos.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPage }, // Inicio
   { path: 'register', component: Register }, // Registro
   { path: 'login', component: Login }, // Login
+  { path: 'calendario', component: CalendarioTurnosComponent }, // Calendario de Turnos
 ];
